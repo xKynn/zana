1c277933cb87e68b87b182c58e6b7c6888a07a4f
Demo <gtownx36@gmail.com>
2022-09-02 03:14:22 -0400

    Added privacy policy to command and github

--- cogs/poe.py
+++ cogs/poe.py
@@ -941,9 +941,8 @@ class PathOfExile(Cog):
     async def privacypolicy(self, ctx):
         """ Zana's Privacy Policy """
         try:
-            await ctx.author.send("""```
-Privacy Policy
-The application "Zana" can collect some surface level server data. The Data collected is limited to Discord server ID values and anonymous error reports. Additionally, error reports are completely opt-in and are confirmed by the user each time before sending an error report. The error reports are completely anonymous and omit any detail in the message and collect only the parsed data the bot uses for a command. An entry for server IDs are only created if the server owner sets up server specific settings. Use of the Bot is considered an agreement to the terms of this Policy.
+            await ctx.author.send("""```Privacy Policy
+The application "Zana" can collect some surface level server data. The Data collected is limited to Discord server ID values and anonymous error reports. Error reports are opt-in and are confirmed by the user each time before sending an error report. The error reports are fully anonymous and omit any detail in the message and collect only the parsed data the bot uses for a command. An entry for server IDs are only created if the server owner sets up server specific settings. Use of the Bot is considered an agreement to the terms of this Policy.
 
 Access to Data
 Access to Data is only permitted to Bot's developers, and only in the scope required for the development, testing, and implementation of features for Bot. Data is not sold, provided to, or shared with any third party, except where required by law or a Terms of Service agreement. You can view the data upon request from the contacts below.
@@ -956,16 +955,13 @@ User Rights
 At any time, you have the right to request to view the Data pertaining to your Discord server. You may submit a request personally to any of the contacts mentioned above. You have the right to request the removal of relevant Data.
 
 Underage Users
-The use of the Bot is not permitted for minors under the age of 13, or under the age of legal consent for their country. This is in compliance with the Discord Terms of Service. No information will be knowingly stored from an underage user. If it is found out that a user is underage we will take all necessary action to delete the stored data.
+The use of the Bot is not permitted for minors under the age of 13, or under the age of legal consent for their country. This is in compliance with Discord Terms of Service. No information will be knowingly stored from an underage user. If it is found out that a user is underage we will take all necessary action to delete the stored data.
 
 Questions
-If you have any questions contact @Demo#7645 on Discord, @xKynn on GitHub or yadav1aryan@gmail.com. For more information check the Discord Terms Of Service.
-```
-        """)
+If you have any questions contact @Demo#7645 on Discord, @xKynn on GitHub or yadav1aryan@gmail.com. For more information check the Discord Terms Of Service.```""")
         except:
-            await ctx.send("""```
-Privacy Policy
-The application "Zana" can collect some surface level server data. The Data collected is limited to Discord server ID values and anonymous error reports. Additionally, error reports are completely opt-in and are confirmed by the user each time before sending an error report. The error reports are completely anonymous and omit any detail in the message and collect only the parsed data the bot uses for a command. An entry for server IDs are only created if the server owner sets up server specific settings. Use of the Bot is considered an agreement to the terms of this Policy.
+            await ctx.send("""```Privacy Policy
+The application "Zana" can collect some surface level server data. The Data collected is limited to Discord server ID values and anonymous error reports. Error reports are opt-in and are confirmed by the user each time before sending an error report. The error reports are fully anonymous and omit any detail in the message and collect only the parsed data the bot uses for a command. An entry for server IDs are only created if the server owner sets up server specific settings. Use of the Bot is considered an agreement to the terms of this Policy.
 
 Access to Data
 Access to Data is only permitted to Bot's developers, and only in the scope required for the development, testing, and implementation of features for Bot. Data is not sold, provided to, or shared with any third party, except where required by law or a Terms of Service agreement. You can view the data upon request from the contacts below.
@@ -973,16 +969,15 @@ Access to Data is only permitted to Bot's developers, and only in the scope requ
 Storage of Data
 Server IDs are stored in a JSON file. Error reports are completely voluntary and made directly through Discord and go directly to @Demo#7645 only. The server and Discord are bot secured to prevent external access through 2-Factor Authentication and other measures.
 
+
 User Rights
 At any time, you have the right to request to view the Data pertaining to your Discord server. You may submit a request personally to any of the contacts mentioned above. You have the right to request the removal of relevant Data.
 
 Underage Users
-The use of the Bot is not permitted for minors under the age of 13, or under the age of legal consent for their country. This is in compliance with the Discord Terms of Service. No information will be knowingly stored from an underage user. If it is found out that a user is underage we will take all necessary action to delete the stored data.
+The use of the Bot is not permitted for minors under the age of 13, or under the age of legal consent for their country. This is in compliance with Discord Terms of Service. No information will be knowingly stored from an underage user. If it is found out that a user is underage we will take all necessary action to delete the stored data.
 
 Questions
-If you have any questions contact @Demo#7645 on Discord, @xKynn on GitHub or yadav1aryan@gmail.com. For more information check the Discord Terms Of Service.
-```
-                    """)
+If you have any questions contact @Demo#7645 on Discord, @xKynn on GitHub or yadav1aryan@gmail.com. For more information check the Discord Terms Of Service.```""")
         env_emoji = '📩'
         try:
             await ctx.message.add_reaction(env_emoji)
